4fcfad7005814fba4fd29826bb671384229a6494
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-03-18 10:26:20 +0100

    ngOnDestroy

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price',
   templateUrl: './price.component.html',
   styleUrl: './price.component.css',
 })
-export class PriceComponent {}
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() price: number = 0;
+
+  public interval$?: Subscription; // El $ es una convencion para saber que es un observable
+
+  ngOnInit(): void {
+    console.log('PriceComponent - ngOnInit');
+    this.interval$ = interval(1000).subscribe((value) => {
+      console.log('tick: ', value);
+    }); //! Observable que activo, hasta que no lo desactive en el ngOnDestroy, seguira activo
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('PriceComponent - ngOnChanges');
+    console.log({ changes });
+  }
+  ngOnDestroy(): void {
+    // Destruir los observables y los listeners que esten activos
+    console.log('PriceComponent - ngOnDestroy');
+    this.interval$?.unsubscribe(); //! Desactivamos el observable
+  }
+}
--- src/app/products/pages/product/product.component.ts
+++ src/app/products/pages/product/product.component.ts
@@ -1,8 +1,65 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'products-product',
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
-export class ProductComponent {}
+export class ProductComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  public isProductVisible: boolean = false;
+  public currentPrice: number = 10;
+  constructor() {
+    console.log('1. constructor');
+  }
+
+  increasePrice() {
+    this.currentPrice++;
+  }
+
+  ngOnInit(): void {
+    console.log('2. ngOnInit');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('3. ngOnChanges');
+    console.log({ changes });
+  }
+  ngDoCheck(): void {
+    console.log('4. ngDoCheck');
+  }
+  ngAfterContentInit(): void {
+    console.log('5. ngAfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('6. ngAfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('7. ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('8. ngAfterViewChecked');
+  }
+  ngOnDestroy(): void {
+    console.log('9. ngOnDestroy');
+  }
+}
